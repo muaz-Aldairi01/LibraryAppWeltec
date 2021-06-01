9fde2d45d1969e3697f167746f465670f1202f87
Dylan Su <Dylan.Su01@studentweltecac.onmicrosoft.com>
2021-06-02 11:43:36 +1200

    Added excel files

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace LibraryAppWeltec
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LibraryAppForm());
+            Application.Run(new librarianForm());
         }
     }
 }
