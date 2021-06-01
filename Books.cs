59652db188e01e115f6cb068bd8daf01bdbf81f0
muaz-aldairi <59302571+muaz-aldairi@users.noreply.github.com>
2021-06-02 10:32:33 +1200

    Added Book class

---
+++ Books.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAppWeltec
+{
+    class Books
+    {
+        //Fields
+        private string bookName;
+        private double price;
+        private string isbn;
+        private string author;
+        private int publishedYear;
+        
+
+        //properties
+        public string BookName { get => bookName; set => bookName = value; }
+        public double Price { get => price; set => price = value; }
+        public string Isbn { get => isbn; set => isbn = value; }
+        public string Author { get => author; set => author = value; }
+
+        public int PublishedYear { get => publishedYear; set => publishedYear = value; }
+
+    }
+}
